2f4b2cf50e2cf92b651052c2b2d58ab9c4d838b7
The Bitcoin Professor <contact@thebitcoinprof.com>
2022-08-12 10:29:48 +0200

    Adding a bitcoin-cli.sh script to make using the node easier.
    Added more documentation, including instruction for testing and using the node

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -1,5 +1,6 @@
+set -e
 
-echo "Downloading latest version of the pruned blockchain..."
+echo "Downloading the latest version of the pruned blockchain..."
 
 curl -L -o /tmp/blockchain.zip https://prunednode.today/latest.zip
 unzip /tmp/blockchain.zip
